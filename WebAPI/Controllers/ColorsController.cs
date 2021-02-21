2b5dc106658a7b80857e71da91215ebff8be776e
ALI OZTURK <aozturk1211@posta.pau.edu.tr>
2021-02-21 18:12:33 +0300

    21/02/2021 Ödev 11.1

--- WebAPI/Controllers/ColorsController.cs
+++ WebAPI/Controllers/ColorsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
+using Entities.Concrete;
 using System.Linq;
 using System.Threading.Tasks;
 
