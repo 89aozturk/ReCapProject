c8b289e232a9f17b271ffbab16275a2a249b484f
ALI OZTURK <aozturk1211@posta.pau.edu.tr>
2021-02-28 22:48:55 +0300

    28/02/2021 Ödev 13

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,99 @@
+﻿using Business.Abstract;
+using Business.Constans;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
+using Core.Utilities.Helpers;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IResult Add(IFormFile file, CarImage carImage)
+        {
+            IResult result = BusinessRules.Run(CheckIfImageLimit(carImage.CarId));
+            if (result!=null)
+            {
+                return result;
+            }
+            carImage.ImagePath = FileHelper.Add(file);
+            carImage.CarImageDate = DateTime.Now;
+            _carImageDal.Add(carImage);
+            return new SuccessResult(Messages.AddedCarImage);
+
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IResult Delete(CarImage carImage)
+        {
+            FileHelper.Delete(carImage.ImagePath);
+            _carImageDal.Delete(carImage);
+            return new SuccessResult(Messages.DeletedCarImage);
+
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IDataResult<CarImage> Get(int id)
+        {
+            return new SuccessDataResult<CarImage>(_carImageDal.Get(c => c.CarImageId == id));
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll());
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IDataResult<List<CarImage>> GetImagesByCarId(int id)
+        {
+            
+            return new SuccessDataResult<List<CarImage>>(CheckIfCarImageNull(id),Messages.CarImageNotNull);
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IResult Update(IFormFile file, CarImage carImage)
+        {
+            carImage.ImagePath = FileHelper.Update(_carImageDal.Get(c => c.CarImageId == carImage.CarImageId).ImagePath, file);
+            carImage.CarImageDate = DateTime.Now;
+            _carImageDal.Update(carImage);
+            return new SuccessResult(Messages.UpdatedCarImage);
+        }
+
+        
+        private IResult CheckIfImageLimit(int id)
+        {
+            
+            return _carImageDal.GetAll(c => c.CarId == id).Count() >= 5 ? new Result(false) : new SuccessResult(Messages.SuccedCarImageAdd);
+           
+        }
+
+        private List<CarImage> CheckIfCarImageNull(int id)
+        {
+            string path = @"\Images\logo.jpg";
+            var result = _carImageDal.GetAll(c => c.CarId == id).Any();
+            if (!result)
+            {
+                return new List<CarImage> { new CarImage { CarId = id, ImagePath = path, CarImageDate = DateTime.Now } };
+            }
+            return _carImageDal.GetAll(p => p.CarId == id);
+            
+        }
+    }
+}
--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -10,9 +10,14 @@ namespace Business.Constans
         public static string CarAdded = "Car Added.";
         public static string CarNameInvalid = "Car name is invalid.";
         public static string DailyPriceInvalid = "DailyPrice of Car must be maximum 0.";
-        internal static string MaintenanceTime="System maintenance";
-        internal static string CarUpdated="Car Updated";
-        internal static string RentalInvalid="Rental Invalid!!";
-        internal static string Rent_A_Car="Rent A Car";
+        public static string MaintenanceTime="System maintenance";
+        public static string CarUpdated="Car Updated";
+        public static string RentalInvalid="Rental Invalid!!";
+        public static string Rent_A_Car="Rent A Car";
+        public static string AddedCarImage="Araba resmi eklendi.";
+        public static string SuccedCarImageAdd="Araç resmi ekleme başarılı.";
+        public static string DeletedCarImage="Araba resmi silindi.";
+        public static string UpdatedCarImage="Araba resmi güncellendi.";
+        public static string CarImageNotNull="Araba resmi var.";
     }
 }
